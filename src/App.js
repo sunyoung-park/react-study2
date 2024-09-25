2cf71aed7b8528cc979ebbce047449b1abd581ce
sunyoung-park <51068439+sunyoung-park@users.noreply.github.com>
2024-09-25 17:19:22 +0900

    router add

--- src/App.js
+++ src/App.js
@@ -1,43 +1,19 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await(
-      await fetch(
-      'https://yts.mx/api/v2/list_movies.json?minimum_rating=9.5&sort_by=year'
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  }
-  useEffect(() => {
-    getMovies();
-  }, []);
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : ( 
-      <div>
-        {movies.map((movie) => (
-          <div key={movie.id}>
-            <img src={movie.medium_cover_image} />
-            <h2>{movie.title}</h2>
-            <p>{movie.summary}</p>
-            <ul>
-              {movie.genres.map((g) => (
-                <li key={g}>{g}</li>
-              ))}
-            </ul>
-          </div>
-          ))}
-      </div>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Routes>
+    </Router> 
   );
 }
 
